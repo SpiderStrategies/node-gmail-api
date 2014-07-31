1904d75b4472114b97dbe4bba8bcda4bdc6ad609
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-07-31 13:33:39 -0400

    Adds ability to fetch meesages for some given query

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ node-gmail-api
 ==============
 
 Node module to interact with the gmail api
+
+Why not the [google official library](https://github.com/google/google-api-nodejs-client)? Well it does too much and doesn't implement batching.
+Which means fetching a bunch of email is insanely painful. This module exposes a function which will query the api searching for messages and hit the google
+batch api to fetch all the messages that are returned.
+
---
+++ index.js
@@ -0,0 +1,71 @@
+var request = require('request')
+  , Parser = require('./lib/parser')
+  , split = require('split')
+  , multiparty = require('multiparty')
+  , ss = require('stream-stream')
+  , api = 'https://www.googleapis.com'
+
+var Gmail = function (key) {
+  if (!key) {
+    throw new Error('Access key required')
+  }
+  this.key = key
+}
+
+/*
+ * Fetches email that matches the query. Returns a stream of messages
+ *
+ * e.g. to search an inbox: gmail.messages('label:inbox')
+ */
+Gmail.prototype.messages = function (q) {
+  var key = this.key
+    , result = ss()
+
+  request({
+    url: api + '/gmail/v1/users/me/messages',
+    json: true,
+    qs: {
+      q: q
+    },
+    headers: {
+      'Authorization': 'Bearer ' + key
+    }
+  }, function (err, response, body) {
+    if (err) {
+      return next(err)
+    }
+    var r = request({
+      method: 'POST',
+      url: api + '/batch',
+      multipart: body.messages.map(function (m) {
+        return {
+          'Content-Type': 'application/http',
+          body: 'GET ' + api + '/gmail/v1/users/me/messages/' + m.id + '\n'
+        }
+      }),
+      headers: {
+        'Authorization': 'Bearer ' + key,
+        'content-type': 'multipart/mixed'
+      }
+    })
+
+    r.on('error', function (e) {
+      parser.emit('error', e)
+    })
+
+    r.on('response', function (res) {
+      var type = res.headers['content-type']
+        , form = new multiparty.Form
+
+      res.headers['content-type'] = type.replace('multipart/mixed', 'multipart/related')
+
+      form.on('part', function (part) {
+        result.write(part.pipe(split('\r\n')).pipe(new Parser))
+      }).parse(res)
+    })
+  })
+
+  return result.pipe(new Parser({objectMode: true}))
+}
+
+module.exports = Gmail
